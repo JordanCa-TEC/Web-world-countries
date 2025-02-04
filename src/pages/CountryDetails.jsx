16395f2cd8fc42b8bb86af90b5d3e8117b580553
email <jordanmorel.ca@gmail.com>
2025-02-03 22:48:53 -0500

    Detalle del pais, terminado

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCountryDetails } from "../redux/slices/countriesSlice";
 import { useParams } from "react-router-dom";
+import "../styles/details.scss"
 
 const CountryDetails = () => {
   const { code } = useParams();
@@ -35,6 +36,8 @@ const CountryDetails = () => {
     currencies,
   } = countryDetails;
 
+  const googleMapUrl = `https://www.google.com/maps?q=${encodeURIComponent(name?.common)}&output=embed`;
+
   return (
     <div className="country-details">
       <h1>{name?.common || "Nombre no disponible"}</h1>
@@ -46,8 +49,19 @@ const CountryDetails = () => {
       <p><strong>Región:</strong> {region || "N/A"}</p>
       <p><strong>Idiomas:</strong> {languages ? Object.values(languages).join(", ") : "N/A"}</p>
       <p><strong>Monedas:</strong> {currencies ? Object.values(currencies).map((c) => c.name).join(", ") : "N/A"}</p>
+
+      {/* Mapa sin API */}
+      <div className="map-container">
+        <iframe
+          src={`https://www.google.com/maps?q=${name?.common}&output=embed`}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </div>
   );
 };
 
 export default CountryDetails;
+
